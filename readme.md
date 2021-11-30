8bc4af3e2ad90beddbe28c72a973b393d7c4f044
Roji89 <roja.gh89@gmail.com>
2021-11-30 11:54:32 +0100

    add database in flask

---
+++ readme.md
@@ -0,0 +1,6 @@
+Hello Guys
+
+add all the commands that you use here to share our knowledge!
+
+# add db to flask
+flask init-db
---
+++ yoga.py
@@ -0,0 +1,40 @@
+from flask import Flask
+import sqlite3
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+app = Flask(__name__)
+
+DATABASE = 'Python.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('Python.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.cli.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    app.cli.add_command(init_db_command)
