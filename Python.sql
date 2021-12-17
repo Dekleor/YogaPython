28d9966ee16e80ec7bb6c168e4cc92a712615b63
Dekleor <67908404+Dekleor@users.noreply.github.com>
2021-12-17 13:43:52 +0100

    Update Python.sql
    modification des position yoga et passage EN

--- Python.sql
+++ Python.sql
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS Cours(
 	
 CREATE TABLE IF NOT EXISTS PositionYoga(
 	nom varchar NOT NULL
-	,description varchar NOT NULL
+	,description longtext NOT NULL
 	,photo varchar
 );
 
@@ -82,11 +82,11 @@ INSERT INTO Cours (IDheure,IDdate,ville,place,IDyoga ) VALUES
 	,((SELECT (heure) FROM TempsHeure WHERE TempsHeure.rowid = 3),(SELECT (DateJ) FROM TempsHeure WHERE TempsHeure.rowid = 3),"Bretagne",25,(SELECT (nomYoga) FROM typeYoga WHERE typeYoga .rowid = 3))
 	,((SELECT (heure) FROM TempsHeure WHERE TempsHeure.rowid = 4),(SELECT (DateJ) FROM TempsHeure WHERE TempsHeure.rowid = 4),"Marseille",10,(SELECT (nomYoga) FROM typeYoga WHERE typeYoga .rowid = 4));
 	
-INSERT INTO PositionYoga(nom,description,photo) VALUES
-	("Panca", "Allongée à plat ventre sur le sol, les jambes écartées de la largeur des hanches", "")
-	,("Uttita Hasta Padangusthana", "Solidement campé sur vos jambes, Placez les mains sur les hanches et fléchissez légerement le genou gauche", "")
-	,("Utthita Parsvakonasana", "Les pieds écartés, pliez le genou droit, le pied tourné à 90 degrés. Etirez la colonne vers la droite en venant poser la main droite à plat sur le tapis", "")
-	,("Prasarita Padottanasana", "Les jambes bien écartées, les bras tendus. Basculez les épaules en arrière, baissez les bras et joignez les mains dans le dos", "")
-	,("Supta Konanasa", "Allongez vous au sol, en restant tonique. Pivotez les bras vers vous, en posant les mains à plat sur le tapis, à coté des cuisses.", "")
+INSERT INTO YogaPose(nom,description,photo) VALUES
+	("Utkatasana", "Start in Mountain Pose. As you inhale, raise your arms, spread your fingers, and reach up through your fingertips. As you exhale, sit back and down as if sitting into a chair. Shift your weight toward the heels and lengthen up through the spine. As you inhale, lift and lengthen through your arms. As you exhale, sit deeper into the pose.", "")
+	,("Uttana shishosana", "Place your hands on the back of a chair with palms shoulder-distance apart. Step your feet back until they align under hips, creating a right angle with your body, spine parallel with the floor. Ground through your feet and lift through thighs. Reach hips away from hands to lengthen the sides of your torso. Firm your outer arms in and lengthen through the crown of your head.", "")
+	,("Adho Mukha Svanasana", "From all fours, walk your hands 6 inches in front of you. Tuck your toes and lift your hips up and back to lengthen your spine. If your hamstrings are tight, keep your knees bent in order to bring your weight back into the legs", "")
+	,("Virabhadrasana II", "Stand with feet wide, 3–4 feet apart. Shift your right heel out so your toes are pointing slightly inward. Turn your left foot out 90 degrees. Line up your left heel with the arch of your right foot. Bend your left knee to a 90-degree angle, keeping the knee in line with the second toe to protect the knee joint. Stretch through your straight back leg and ground down into the back foot.", "")
+	,("Trikonasana", "Stand with feet wide, 3–4 feet apart. Shift your right heel out so your toes are pointing slightly inward. Turn your left foot out 90 degrees. Line up your left heel with the arch of your right foot. Keeping both legs straight, ground through your feet. Lift arms into a T at shoulder height. Reach forward with your front arm. When you can’t reach anymore, hinge at the front hip.", "")
 	
 COMMIT TRANSACTION;
